c2b0ce3b313c4e37b8c2ab73db672df704229832
ajask1259 <taiko.vita@gmail.com>
2021-04-26 05:17:56 +0900

    課題前完了

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //SVProgressHUDをxcode11以上で実行するためのコード
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
---
+++ instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  instagram
+//
+//  Created by 荒井竣哉 on 2021/04/26.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
